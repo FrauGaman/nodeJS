ffae49dee9d8930eb1f688b068395d18dd6e938d
margarita <gaman@magora-systems.com>
2020-02-06 11:35:31 +0700

    [*] weather-app https usage

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,15 +2,15 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/3f0b3a15989f2ffab4cc663760d9db2e/${latitude}, ${longitude}`;
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location service', undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location. Try another search', undefined);
         } else {
             callback(undefined, {
-                temperature: response.body.currently.temperature,
-                precipProbability: response.body.currently.precipProbability,
+                temperature: body.currently.temperature,
+                precipProbability: body.currently.precipProbability,
             })
         }
     })
