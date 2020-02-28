279bdf457570f1db853dc4047c0473f0892d70e4
Margarita Gaman <Gamanm.Web@yandex.ru>
2020-02-28 13:43:10 +0700

    [*] task-manager add tasks sorting

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -20,11 +20,17 @@ router.post('/tasks', auth, async (req, res) => {
 
 //GET /tasks?completed=false
 //GET /tasks/limit=10&skip=0
+//GET /tasks?sortBy=createdAt
 router.get('/tasks', auth, async (req, res) => {
     const match = {};
+    const sort = {};
 
     if(req.query.completed) {
         match.completed = req.query.completed ==='true';
+    }
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
 
     try {
@@ -42,7 +48,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate();
 
